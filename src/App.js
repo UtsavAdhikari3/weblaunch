9fdce7d1e90fe9cfe53e6fab5a7489f0de0ff2a5
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2022-10-14 08:09:36 +0545

    weblauch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button';
+import button from './Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+
+    return(
+    <div>
+        <h1>My first website</h1>
+        <h3>You can open any website you want to.</h3>
+        <Button name="LinkedIn" link="https://linkedin.com"/>
+        <Button name="Youtube" link="https://youtube.com"/>
+        <Button name="Facebook" link="https://facebook.com"/>
+        <Button name="GitHub" link="https://github.com"/><br/>
+
     </div>
-  );
+    )
 }
-
 export default App;
